a5762595f9d562bcf1e96e9f47ae306a6639262d
Luke Wilson <wilson.palooza@gmail.com>
2016-11-05 16:08:33 -0700

    - Changes to server in preparation for deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-var express = require ('express');
-
+var express = require('express');
+var path = require('path');
 var app = express()
 
+//serve static files
 app.use(express.static('../public'));
 
 // do we need this route for index.html? I'm guessing no.
-// app.get('/', function(req,res) {
-//   res.render('/index.html')
-// })
+app.get('/', function(req,res) {
+  res.render('/index')
+})
 
-app.listen(process.env.PORT || 3002, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('server is running')
 });
+
